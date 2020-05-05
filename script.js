9ae7bf3379282d6be4948626abf74ee1aabe0270
Stanley Dukor <stanleydukor@gmail.com>
2020-05-05 17:26:27 +0100

    Fix timer issue, add array loop with timer and add unordered list

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 
     $("#searchBtn").on("click", function () {
         var searchVal = $("#citySearch").val();
-
-
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&appid=1111c693bffa0cd8263c73cbe8fe55be&units=imperial";
 
         $.ajax({
@@ -13,12 +11,10 @@ $(document).ready(function () {
          }).then(function (response) {
             console.log(response)
 
-            $("#location1").append(response.name)
-            $("#temperature2").append(response.main.temp)
-            $("#humidity3").append(response.main.humidity)
-            $("#windSpeed4").append(response.wind.speed)
-
-
+            $("#location1").html(response.name)
+            $("#temperature2").html(response.main.temp)
+            $("#humidity3").html(response.main.humidity)
+            $("#windSpeed4").html(response.wind.speed)
 
             var fiveDayurl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=1111c693bffa0cd8263c73cbe8fe55be&units=imperial";
             $.ajax({
@@ -26,18 +22,20 @@ $(document).ready(function () {
                 type: "GET",
                 dataType: "json",
              }).then(function (response) {
-                 response = JSON.stringify(JSON_DATA);
-                console.log(response)
-                console.log(response.list[0].dt_txt)
-                $("#Temp1").html(response.list[0]);
-                $("#hum").html(time)
+                var len = response.list.length
+                for (let i=0; i<5; i++) {
+                    setTimeout(() => {
+                        var output = response.list[i]
+                        
+                        // var li = document.createElement("li");
+                        // var text = document.createTextNode(output.dt_txt);
+                        // li.appendChild(text);
+                        // document.getElementById("Temp1").appendChild(li);
 
-
-
-
-
-
-
+                        $("#Temp1").html(output.dt_txt);
+                        $("#hum").html(output.main.humidity);
+                    }, i*1000);
+                }
             })
 
         })
