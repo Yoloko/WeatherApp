442006c1219c95c3028c906c3ba0d4eb17aea782
yetunde oloko <faithholoko@gmail.com>
2020-05-06 13:25:25 -0400

    fix

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
     $("#searchBtn").on("click", function () {
+
         var searchVal = $("#citySearch").val();
+        localStorage.setItem("searchVal", searchVal);
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&appid=1111c693bffa0cd8263c73cbe8fe55be&units=imperial";
 
         $.ajax({
@@ -23,10 +25,12 @@ $(document).ready(function () {
                 dataType: "json",
             }).then(function (response) {
                 console.log(response)
+
                 var output = response.list[1]
                 $("#date1").html(output.dt_txt)
+                $(".img").attr("src",output.weather[0].icon);
                 $("#hum1").text(" Humidity "+ output.main.humidity);
-                $("#temp1").text("Temp" + output.main.temp);
+                $("#temp1").text(" Temp " + output.main.temp);
 
                 var output = response.list[9]
                 $("#date2").html(output.dt_txt);
@@ -35,18 +39,18 @@ $(document).ready(function () {
 
                 var output = response.list[17]
                 $("#date3").html(output.dt_txt);
-                $("#hum3").html("humidity"+output.main.humidity);
-                $("#temp3").text("Temp"+ output.main.temp);
+                $("#hum3").text(" Humidity "+ output.main.humidity);
+                $("#temp3").text(" Temp "+ output.main.temp);
 
                 var output = response.list[25]
                 $("#date4").html(output.dt_txt);
-                $("#hum4").text("humidity"+ output.main.humidity);
-                $("#temp4").text("Temp"+ output.main.temp);
+                $("#hum4").text(" Humidity "+ output.main.humidity);
+                $("#temp4").text(" Temp " + output.main.temp);
 
                 var output = response.list[33]
-                $("#date5").html(output.dt_txt.moment);
-                $("#hum5").text("Humidity" + output.main.humidity);
-                $("#temp5").text("Temp"+ output.main.temp);
+                $("#date5").html(output.dt_txt);
+                $("#hum5").text(" Humidity " + output.main.humidity);
+                $("#temp5").text(" Temp "+ output.main.temp);
 
 
 
